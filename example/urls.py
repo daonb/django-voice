28bb53effc37499a38d5c6e1e12edefc779eeea6
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2011-07-03 00:08:18 +0300

    fix #6

--- example/urls.py
+++ example/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'example.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^feedback/', include('djangovoice.urls')))
