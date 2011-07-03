c7e5d9c56973223ed7d1903444bb506f4a674a03
Gökmen Görgen <gkmngrgn@gmail.com>
2011-07-03 11:51:30 +0300

    use reverse function for getting feedback page url.

--- example/views.py
+++ example/views.py
@@ -1,11 +1,13 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 
-def home(request):
-    # TODO: target of url of feedback is hardcoded, make it use 
-    # django.core.urlresolvers.reverse.
-    return HttpResponse("""
+template = """\
 <h1 style="text-align:center">Say hello to my App!</h1>
+<script type="text/javascript" src="/static/djangovoice/js/djangovoice.js"></script>
+<p style="text-align:center">
+    Do you want to give feedback to my app? Than you can use <a href="%(feedback_url)s">feedback</a>.
+</p>
+"""
 
-<p style="text-align:center">Do you want to give feedback to
-my app? Than you can use <a href="/feedback/">feedback</a></p>
-""")
+def home(request):
+    return HttpResponse(template % {'feedback_url': reverse('djangovoice_home')})
