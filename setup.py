e5e5447b323db6621e564627d40ef05a0304ba53
Gökmen Görgen <gkmngrgn@gmail.com>
2011-08-26 21:53:36 +0300

    exclude demo application. some fixes in django-voice.

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import djangovoice
 from setuptools import find_packages
 from setuptools import setup
-from djangovoice import get_version
 
-LONG_DESCRIPTION = """
-============
-Django Voice
-============
-
-This application lets you solicit feedback and suggestions from
-your users, who can then vote and comment on other suggestions.
-"""
+readme_file = 'README.rst'
+try:
+    long_description = file(readme_file).read()
+except IOError, err:
+    sys.stderr.write("[ERROR] Cannot find file specified as "
+        "``long_description`` (%s)\n" % readme_file)
+    sys.exit(1)
 
 setup(
     name='django-voice',
-    version=get_version(),
+    version=djangovoice.get_version(),
     description="A feedback application for Django 1.3 or later",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Environment :: Web Environment",
-        ],
+    ],
     keywords='django,feedback,discussion',
     author='Gökmen Görgen',
     author_email='gokmen@alageek.com',
-    url='https://github.com/negzel/django-voice',
+    url='https://github.com/alageek/django-voice',
+    download_url='https://github.com/alageek/django-voice/downloads',
     license='BSD',
-    packages=find_packages(),
+    packages=find_packages(exclude=('demo', 'demo.*')),
     include_package_data=True,
     zip_safe=False,
     install_requires=['setuptools'],
-    )
+)
