0298335d63d2a9b8837a03e88d64156d06a680e9
Gökmen Görgen <gkmngrgn@gmail.com>
2011-08-26 23:41:31 +0300

    new version

--- djangovoice/__init__.py
+++ djangovoice/__init__.py
@@ -2,7 +2,7 @@ from django.template import add_to_builtins
 
 add_to_builtins('django.templatetags.i18n')
 
-VERSION = (0, 2)
+VERSION = (0, 3)
 
 def get_version():
     return '.'.join(map(str, VERSION))
